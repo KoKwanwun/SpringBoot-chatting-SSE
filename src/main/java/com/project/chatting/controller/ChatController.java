37b63f6bd7a6093182dfa794a9757226f1f74a40
KoKwanwun <dkfajflsla@naver.com>
2022-12-19 10:57:49 +0900

    [Update] GET /chat/messages 추가 및 LocalDateTime 표시형식 변경

--- src/main/java/com/project/chatting/controller/ChatController.java
+++ src/main/java/com/project/chatting/controller/ChatController.java
@@ -3,6 +3,7 @@ package com.project.chatting.controller;
 import com.project.chatting.domain.ChatMessage;
 import com.project.chatting.domain.RSData;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,10 +26,22 @@ public class ChatController {
 
         ChatMessage message = new ChatMessage("홍길동", "안녕하세요.");
 
-        return new RSData(
+        chatMessages.add(message);
+
+        return new RSData<>(
                 "S-1",
                 "메세지가 작성되었습니다",
                 new writeMessageResponse(message.getId()));
+    }
+
+    @GetMapping("/messages")
+    @ResponseBody
+    public RSData<List<ChatMessage>> messages() {
+        return new RSData<>(
+                "S-1",
+                "성공",
+                chatMessages
+        );
     }
 
 }
