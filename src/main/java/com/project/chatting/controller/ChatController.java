eff19cddfc699dc932a67b35ca6fdb797269123f
KoKwanwun <dkfajflsla@naver.com>
2022-12-19 11:21:47 +0900

    [Update] messages filter하여 조회

--- src/main/java/com/project/chatting/controller/ChatController.java
+++ src/main/java/com/project/chatting/controller/ChatController.java
@@ -2,11 +2,13 @@ package com.project.chatting.controller;
 
 import com.project.chatting.domain.ChatMessage;
 import com.project.chatting.domain.RSData;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/chat")
@@ -34,13 +36,36 @@ public class ChatController {
                 new writeMessageResponse(message.getId()));
     }
 
+    public record MessagesRequest(Long fromId){
+    }
+
+    public record MessagesResponse(List<ChatMessage> messages, long count){
+    }
+
     @GetMapping("/messages")
     @ResponseBody
-    public RSData<List<ChatMessage>> messages() {
+    public RSData<MessagesResponse> messages(MessagesRequest request) {
+        List<ChatMessage> messages = chatMessages;
+
+        // 번호가 입력되었다면
+        if(request.fromId != null){
+            // 해당 번호의 채팅메세지가 전체 리스트에서의 배열인덱스 번호를 구함
+            // 없다면 -1
+            int idx = IntStream.range(0, messages.size())
+                    .filter(i -> chatMessages.get(i).getId() == request.fromId)
+                    .findFirst()
+                    .orElse(-1);
+
+            // 만약 존재한다면, 0번 ~ idx번까지 제거한 리스트로 만듦
+            if(idx != -1){
+                messages = messages.subList(idx + 1, messages.size());
+            }
+        }
+
         return new RSData<>(
                 "S-1",
                 "성공",
-                chatMessages
+                new MessagesResponse(messages, messages.size())
         );
     }
 
